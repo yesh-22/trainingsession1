02c93b7c4229c4ee6bfef2e79c0c1aa08b60fbdd
unknown <bhavani@bhavani.com>
2025-01-08 21:31:29 +0530

    day 3 of practice

---
+++ function.js
@@ -0,0 +1,9 @@
+//function declaration statment
+console.log("function declaration statment");
+function demo(){
+
+    console.log("hello-world");
+
+}
+console.log(demo);//returns the whole demo function
+//console.log(demo()) returns the body present in the function
